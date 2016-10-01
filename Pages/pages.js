479e7ef61b4744b95adbfc6c46ca9d89f35fdb11
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-10-01 15:16:41 -0400

    Adding system for managing multiple portfolio pages in one file.

---
+++ Pages/pages.js
@@ -0,0 +1,28 @@
+exports.list = ["dunes", "about"];
+
+exports.pages = {
+
+    oscillators: {
+        fr: {
+            title: "Les Oscillateurs",
+            description: "Un film d'animation en noir et blanc réalisé avec p5.js."
+        },
+        en: {
+            title: "Oscillators",
+            description: "A black and white animation film made with p5.js."
+        }
+        link: "http://www.vimeo.com/pelletierauger"
+    },
+
+    joy_and_confusion: {
+        fr: {
+            title: "Les joies confuses",
+            description: "Un film d'animation coloré réalisé avec p5.js."
+        },
+        en: {
+            title: "Joy and Confusion",
+            description: "A colorful animation film made with p5.js."
+        },
+        link: "http://www.vimeo.com/pelletierauger"
+    }
+};
