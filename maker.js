f4cee8872e60cc5c79c3ffb8bd7ed6b7373b2837
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-10-04 00:55:51 -0400

    Basic CSS. Menu Highlights. No index.html URLs. No about in the mosaic.

--- maker.js
+++ maker.js
@@ -13,14 +13,12 @@ makeIndex(pages, "en", "./en/");
 
 for (var i = 0; i <  pages.list.length; i++) {
     var page = (pages.pages[pages.list[i]] || require('./pages/' + pages.list[i]));
-    // console.log(page);
     makePage(page, "fr");
     makePage(page, "en");
 }
 
 function makePage(page, language) {
     var fileName = filenameFormatter(page[language].title);
-    // console.log(fileName);
     var header = makeHeader(page, language);
     var navigation = makeNavigation(page, language);
     var content = makeContent(page, language);
@@ -28,19 +26,22 @@ function makePage(page, language) {
     makeFile(language, fileName, header + navigation + content + footer);
 }
 
-function makeHeader(page, language) {
-    if (page) {
-        var title = " - " + page[language].title;
-    } else {
-        var title = "";
-    }
+function makeHeader(page, language, prefix) {
+    var title = (page) ? " - " + page[language].title : "";
+    // if (page) {
+    //     var title = " - " + page[language].title;
+    // } else {
+    //     var title = "";
+    // }
+    prefix = (prefix == "./en/") ? "" : ".";
+
     return `<!DOCTYPE html>
     <html>
     <head>
         <meta charset="UTF-8">
         <title>Guillaume Pelletier-Auger${title}</title>
-    </head>
-    `;
+        <link href="${prefix}./style.css" rel="stylesheet" type="text/css">
+    </head>`;
 }
 
 function makeNavigation(page, language, prefix) {
@@ -49,32 +50,35 @@ function makeNavigation(page, language, prefix) {
     var nav1 = (language == "fr") ? "Travaux" : "Works";
     var nav2 = (language == "fr") ? "À propos" : "About";
     var nav3 = (language == "fr") ? "Français -> English" : "English -> Français";
+
+    var liWorks = (page["en"].title == "index") ? `<li class = "selected">` : "<li>";
+    var liAbout = (page["en"].title == "About") ? `<li class = "selected">` : "<li>";
+
     var oppositeLanguage = (language == "fr") ? "en" : "fr";
     var navAbout = (language == "fr") ? "a-propos" : "about";
     var navLang = (language == "fr") ? filenameFormatter(page["en"].title) : filenameFormatter(page["fr"].title);
-    console.log("navLang = " + navLang);
-    var navigation = `<body><h1><a href="../${language}/index.html">Guillaume Pelletier-Auger</a></h1>
+    navLang = (navLang == "index") ? "" : navLang + ".html";
+
+    return `<body><h1><a href="../${language}/">Guillaume Pelletier-Auger</a></h1>
     <div id="nav"><ul>
-    <li><a href="${prefix}${language}/index.html">${nav1}</a></li>
-    <li><a href="${prefix}${language}/${navAbout}.html">${nav2}</a></li>
-    <li><a href="${prefix}${oppositeLanguage}/${navLang}.html">${nav3}</a></li>
+    ${liWorks}<a href="${prefix}${language}/">${nav1}</a></li>
+    ${liAbout}<a href="${prefix}${language}/${navAbout}.html">${nav2}</a></li>
+    <li><a href="${prefix}${oppositeLanguage}/${navLang}">${nav3}</a></li>
     </ul>
-    </div>
-    `;
-    return navigation;
+    </div>`;
 }
 
 function makeContent(page, language) {
-    var content = `<div id="main">${page[language].content}</div>`;
-    return content;
+    return `<div id="main">
+    <h2>${page[language].title}</h2>
+    ${page[language].content}</div>`;
 }
 
 
 function makeFooter(page, language) {
     return `<div id="footer">Guillaume Pelletier-Auger - 2016</div>
     </body>
-    </html>
-    `;
+    </html>`;
 }
 
 function makeFile(language, fileName, htmlContent) {
@@ -84,7 +88,7 @@ function makeFile(language, fileName, htmlContent) {
         if (err) {
             return console.error(err);
         } else {
-            console.log("Data written successfully!");
+            console.log(fileName + '.html written successfully.');
         }
     });
 }
@@ -92,7 +96,7 @@ function makeFile(language, fileName, htmlContent) {
 //-----------Indexes-----------------------------------------------------------------------//
 
 function makeIndex(pages, language, prefix) {
-    var header = makeHeader();
+    var header = makeHeader(null, null, prefix);
     var navPrefix = (prefix == "./en/") ? "./" : "../";
     var navigation = makeNavigation({
         fr: { title: "index" },
@@ -112,64 +116,25 @@ function makeMosaic(pages, language, prefix) {
     for (var i = 0; i < pages.list.length; i++) {
         var page = (pages.pages[pages.list[i]] || require('./pages/' + pages.list[i]));
 
+        //We exclude the "About" page from building an element in the mosaic.
+        if (page["en"].title == "About") {
+            continue;
+        }
+
         var title = page[language].title;
         var filename = filenameFormatter(page[language].title);
         var thumbnailName = pages.list[i];
         var description = page[language].description;
         var link = (page.link || prefix + filename + ".html");
         var navPrefix = (prefix == "./en/") ? "./" : "../";
-        console.log("Link : " + link);
+
         var itemDiv = `<div class = "portfolio-item"><div class = "thumbnail">
         <a href="${link}"><img src="${navPrefix}images/${thumbnailName}.jpg"></a></div>
         <div class = "portfolio-description">
-        <h2>${title}</h2><p>${description}</p></div>`;
+        <h2>${title}</h2><p>${description}</p></div></div>`;
 
         mosaic = mosaic + itemDiv;
     }
-    mosaic = mosaic + `</div>`;
-    return mosaic;
-}
-
-
-function makeMosaicBeta(pages, language) {
-    //We start writing the HTML content.
-    var mosaic = `<div class="mosaic">`;
-
-    //We loop through all the files.
-    for (var i = 0; i < pages.length; i++) {
-        //We let "page" be the file that is currently processed.
-        var page = require('./pages/' + files[i]);
-
-        if (language == "fr") {
-            var pageLang = page.fr;
-        } else if (language == "en") {
-            var pageLang = page.en;
-        }
-
-        //I reformat the filename using a regular expression.
-        //This is done because the filenames in the "pages" folder are .js,
-        //and I need .html files for the output.
-
-        var r = /([^:\\/]*?)(?:\.([^ :\\/.]*))?$/;
-
-        var formattedName = files[i].match(r)[1];
-
-        //We form the full link, considering also the language argument.
-        //If page.link exists, we pick it. Otherwise, we build the link.
-        var link = (page.link || "./" + language + "/" + formattedName + ".html");
-
-        //Next, we need to build the divs containing the title and description of the portfolio item.
-        var title = pageLang.title;
-        var description = pageLang.description;
-
-        var itemDiv = `<div class = "portfolio-item"><div class = "thumbnail">
-        <a href="${link}"><img src="../images/${formattedName}.jpg"></a></div>
-        <div class = "portfolio-description">
-        <h2>${title}</h2><p>${description}</p></div>`;
-
-        mosaic = mosaic + itemDiv;
-    }
-    //Closing the div of the mosaic
     mosaic = mosaic + `</div>`;
     return mosaic;
 }
