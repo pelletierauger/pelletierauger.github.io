ad5a61ca797b1b225f76d22dea06a87e57938b13
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-10-11 00:40:07 -0400

    Work on the about sections.

--- Pages/about.js
+++ Pages/about.js
@@ -1,44 +1,77 @@
 exports.fr = {
     title: "À propos",
     description: "Guillaume Pelletier-Auger est un artiste.",
-    content: `<i>Guillaume</i> est une série d'images générées par une variété de fonctions itératives.
-    Elles ont été créées avec p5.js.`
+    content: `
+        <div id ="contact">
+            <ul>
+            <li><a href="https://github.com/pelletierauger">GitHub</a></li>
+            <li><a href="https://www.instagram.com/pelletierauger/">Instagram</a></li>
+            <li><a href="https://twitter.com/pelletierauger">Twitter</a></li>
+            <li><a href="https://www.facebook.com/pelletierauger">Facebook</a></li>
+            </ul>
+        </div>
+        <div id ="about">
+        <p>Mes intérêts principaux sont l'art et la programmation.
+
+        J'ai étudié l'animation au Cégep du Vieux Montréal. J'ai ensuite étudié les arts
+        graphiques à l'Académie des Beaux-Arts de Varsovie, en Pologne, me concentrant
+        sur l'illustration avec le professeur Zygmunt Januszewski et sur l'animation
+        avec le professeur Hieronim Neumann.</p>
+
+        <h2>Ce site Web</h2>
+        <p>
+            Pour réaliser ce site Web, j'ai développé un petit module Node.js qui lit une
+            base de données puis génère automatiquement tous les fichiers HTML. J'ai créé
+            ce module parce que je cherchais un <i>générateur de site statique</i> ("static site
+            generator") avec quelques fonctionnalités qui me sont importantes (tel que
+            la possibilité de gérer avec aisance une base de données de contenu bilingue), et
+            j'étais insatisfait par les générateurs déjà existants.
+            <br><br>
+            Vous pouvez voir le code source de ce module Node.js (avec le code source de ce site
+            Web au complet) ici : <a href="https://github.com/pelletierauger/pelletierauger.github.io">
+            https://github.com/pelletierauger/pelletierauger.github.io</a>
+            <br><br>
+            Le module principal est <i>maker.js</i> et il lit les données présentes dans
+            le dossier <i>pages</i>.
+        </p>
+        </div>
+    `
 };
 
 exports.en = {
     title: "About",
     description: "Guillaume Pelletier-Auger is an artist.",
     content: `
+        <div id ="contact">
+            <ul>
+            <li><a href="https://github.com/pelletierauger">GitHub</a></li>
+            <li><a href="https://www.instagram.com/pelletierauger/">Instagram</a></li>
+            <li><a href="https://twitter.com/pelletierauger">Twitter</a></li>
+            <li><a href="https://www.facebook.com/pelletierauger">Facebook</a></li>
+            </ul>
+        </div>
+        <div id ="about">
+        <p>My primary interests are art and programming.
 
-        My primary interests are art and programming.
-
-        I studied animation and illustration.
+        I studied animation at the Cégep du Vieux Montréal. I then studied graphic arts at
+        the Academy of Fine Arts in Warsaw, Poland, concentrating on illustration with professor
+        Zygmunt Januszewski and animation with professor Hieronim Neumann.</p>
 
         <h2>This website</h2>
         <p>
             To make this website, I developed a small Node.js module that reads a database
             and outputs HTML content, generating all the files automatically. 
-            I created this module because I wanted a lightweight 
-            and flexible Static Site Generator and I couldn't find one 
-            that satisfied my taste for simplicity.
-
+            I created this module because I wanted a lightweight static site generator 
+            with a few key features (like the possibility of easily managing a database of
+            bilingual content), and I wasn't satisfied by pre-existing generators.
+            <br><br>
             You can view the source code for this Node.js module here (along with the source code
-            for my whole website) :
-            <a href="https://github.com/pelletierauger/pelletierauger.github.io">
+            for my whole website) : <a href="https://github.com/pelletierauger/pelletierauger.github.io">
             https://github.com/pelletierauger/pelletierauger.github.io</a>
-
+            <br><br>
             The main module is <i>maker.js</i> and it reads the data inside the <i>pages</i> folder.
-
-            I'm tempted to turn this into a generalized module that could be used by other people,
-            but I think this tool is mostly interesting because it has a single purpose
-            (building my own website). It might be more useful to write an article about
-            the concepts behind the tool to encourage others to make similar singe-purpose site
-            builders. I find it exquisite to use a tool that only does what you want, 
-            exactly as you want it, and nothing more. 
-            Also, adding custom-made features as needed is rewarding and introduces zero bloat.
-            If this lightweight site builder were to be <i>generalized</i> into a more fully-featured application,
-            I think it would lose its essential simplicity immediately.
         </p>
+        </div>
     `
 }
 
