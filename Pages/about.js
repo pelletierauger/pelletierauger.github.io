f2d326be6767875bb1c10f67337dd460d5455e51
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-10-14 16:05:45 -0400

    Work on the About page.

--- Pages/about.js
+++ Pages/about.js
@@ -12,21 +12,25 @@ exports.fr = {
             </ul>
         </div>
         <div id ="about">
-        <p>Mes intérêts principaux sont l'art et la programmation.
+        <p>Je suis un artiste et un programmeur vivant à Montréal, Québec, Canada.<br><br>
 
-        J'ai étudié l'animation au Cégep du Vieux Montréal. J'ai ensuite étudié les arts
-        graphiques à l'Académie des Beaux-Arts de Varsovie, en Pologne, me concentrant
+        J'ai d'abord étudié le film d'animation au Cégep du Vieux Montréal, puis l'illustration
+        à l'Académie d'été internationale des Beaux-Arts de Salzbourg, en Autriche. J'ai ensuite étudié
+        les arts graphiques à l'Académie des Beaux-Arts de Varsovie, en Pologne, me concentrant
         sur l'illustration avec le professeur Zygmunt Januszewski et sur l'animation
-        avec le professeur Hieronim Neumann.</p>
+        avec le professeur Hieronim Neumann.<br><br>
 
+        Je travaille autant avec des médiums physiques (encre de Chine, aquarelle, acrylique) qu'avec du
+        code informatique. Ce site Web présente mon art numérique, et vous pouvez découvrir mon art
+        <i>traditionnel</i> sur cet autre site : <a href="http://www.gpelletier.com/">www.gpelletier.com</a>.</p>
         <h2>Ce site Web</h2>
         <p>
             Pour réaliser ce site Web, j'ai développé un petit module Node.js qui lit une
             base de données puis génère automatiquement tous les fichiers HTML. J'ai créé
             ce module parce que je cherchais un <i>générateur de site statique</i> ("static site
-            generator") avec quelques fonctionnalités qui me sont importantes (tel que
-            la possibilité de gérer avec aisance une base de données de contenu bilingue), et
-            j'étais insatisfait par les générateurs déjà existants.
+            generator") à la fois simple et offrant quelques fonctionnalités qui me sont importantes
+            (tel que la possibilité de gérer une base de données de contenu bilingue), et
+            j'étais insatisfait des générateurs déjà existants.
             <br><br>
             Vous pouvez voir le code source de ce module Node.js (avec le code source de ce site
             Web au complet) ici : <a href="https://github.com/pelletierauger/pelletierauger.github.io">
@@ -53,11 +57,17 @@ exports.en = {
             </ul>
         </div>
         <div id ="about">
-        <p>My primary interests are art and programming.
+        <p>I'm an artist and programmer living in Montréal, Québec, Canada.<br><br>
 
-        I studied animation at the Cégep du Vieux Montréal. I then studied graphic arts at
-        the Academy of Fine Arts in Warsaw, Poland, concentrating on illustration with professor
-        Zygmunt Januszewski and animation with professor Hieronim Neumann.</p>
+        I first studied animation film at the Cégep du Vieux Montréal, and then took an illustration 
+        course at the International Summer Academy of Fine Arts in Salzburg, Austria. I then 
+        studied graphic arts at
+        the Academy of Fine Arts in Warsaw, Poland, were I concentrated on illustration with professor
+        Zygmunt Januszewski and animation with professor Hieronim Neumann.<br><br>
+
+        I work as much with physical media (India ink, watercolors, acrylics) as I work with
+        computer code. This website presents my digital art, and you can see my <i>traditional</i>
+        art at this other website: <a href="http://www.gpelletier.com/">www.gpelletier.com</a>.</p>
 
         <h2>This website</h2>
         <p>
