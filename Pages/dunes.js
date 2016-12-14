012084a8058c44dc7e3bafdcc67ee0896af80105
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-12-13 22:21:25 -0500

    Write the French version of Dunes

--- Pages/dunes.js
+++ Pages/dunes.js
@@ -2,30 +2,103 @@ exports.fr = {
     title: "Les Dunes",
     description: "Une série d'image générées par des fonctions itératives.",
     content: String.raw `
-    <div id="page">
-    <p>Cette série d'images a été générée par une variété de fonctions itératives.
-    J'aime les mathématiques et la programmation, et j'ai décidé de m'en servir pour dessiner. 
-    Pour ce projet, j'ai tout d'abord écrit une équation paramétrique dont la courbe ressemble 
-    un peu à une méduse.</p>
+    <div id="page"><p>Ce projet est basé sur
+     <a href="http://koaning.io/fluctuating-repetition.html">Fluctuating Repetition</a>,
+     un article de blog écrit par Vincent D. Warmerdam. Les images ci-dessous ont été générées
+    avec ce système dynamique :</p>
+    <div class="math">
+    \[
+        \begin{align*}
+            x_{n+1} &= \sin(a \times x_n) + \tan(b \times x_n) - \tan z_n\\
+            y_{n+1} &= \sin(c \times x_n) + \sin(d \times z_n)\\
+            z_{n+1} &= z_n + 0.1
+        \end{align*}
+    \]
+    </div>
+    <p>J'ai réglé la valeur de \(x_{0}, y_{0}, z_{0}\) à \(0\), mais le résultat est presque
+    identique peu importe les valeurs initiales. Le système est ensuite itéré plusieurs millions de fois, 
+    et un point blanc d'une très faible opacité est dessiné à chaque itération.</p>
+   
+    <p>Différentes images sont générées lorsque les constantes \(a, b, c, d\) sont changées. On peut voir
+    ci-dessous quatre ensembles de valeurs pour ces constantes, chacun suivi de l'image qu'il génère.</p>
+    
+    <div class="math">
+    \[
+        a = -0.1, b = 1, c = 0.1, d = 1
+    \]
+    </div>
+    <img src="../images/dunes/dunes002b.jpg">
+    <div class="math">
+    \[
+        a = -0.1, b = -1, c = 0.1, d = 3
+    \]
+    </div>
+    <img src="../images/dunes/dunes001b.jpg">
+    <div class="math">
+    \[
+        a = -0.6, b = 1, c = 0.1, d = 1
+    \]
+    </div>
+    <img src="../images/dunes/dunes003b.jpg">
+    <div class="math">
+    \[
+        a = -0.3, b = 1, c = 0.12, d = 3
+    \]
+    </div>
+    <img src="../images/dunes/dunes004.jpg">
+    <p>Here's the complete code for the project, written in JavaScript with p5.js:</p>
+        <pre><code>var duneOne = {
+    a: -0.1,
+    b: 1,
+    c: 0.1,
+    d: 1
+};
 
-    <pre><code>function makePage(page, language) {
-    var fileName = filenameFormatter(page[language].title);
-    makeFile(language, fileName, header + navigation + content + footer);
+var duneTwo = {
+    a: -0.1,
+    b: -1,
+    c: 0.1,
+    d: 3
+};
+
+var duneThree = {
+    a: -0.6,
+    b: 1,
+    c: 0.1,
+    d: 1
+};
+
+var duneFour = {
+    a: -0.3,
+    b: 1,
+    c: 0.12,
+    d: 3
+};
+
+var vals = duneFour;
+var vec = new p5.Vector(0, 0, 0);
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+    fill(255, 5);
+    noStroke();
+}
+
+function draw() {
+    translate(width / 2, height / 2);
+    for (var i = 0; i < 1500; i++) {
+        vec = iterative(vec.x, vec.y, vec.z);
+        ellipse(vec.x * 70, vec.y * 200, 0.5, 0.5);
+    }
+}
+
+function iterative(x, y, z) {
+    var newX = sin(vals.a * x) + tan(vals.b * x) - tan(z);
+    var newY = sin(vals.c * x) + sin(vals.d * z);
+    var newZ = z + 0.1;
+    return createVector(newX, newY, newZ);
 }</code></pre>
-
-    <img src="../images/dunes/dunes001.jpg">
-    <img src="../images/dunes/dunes002.jpg">
-    <img src="../images/dunes/dunes003.jpg">
-
-    <pre><code>function makePage(page, language) {
-    var fileName = filenameFormatter(page[language].title);
-    var header = makeHeader(page, language);
-    var navigation = makeNavigation(page, language);
-    var content = makeContent(page, language);
-    var footer = makeFooter(page, language);
-    makeFile(language, fileName, header + navigation + content + footer);
-}</code></pre>
-
     </div>
     `
 };
@@ -131,6 +204,6 @@ function iterative(x, y, z) {
 }</code></pre>
     </div>
     `
-}
+};
 
 exports.link = null;
