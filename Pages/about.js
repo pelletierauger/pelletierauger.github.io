037d86b47e6d40b58eed6dc4da3acd3a3879504e
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-10-17 15:14:02 -0400

    Work on the French about page

--- Pages/about.js
+++ Pages/about.js
@@ -10,14 +10,39 @@ exports.fr = {
             </ul>
         </div>
         <div id ="about">
-        <p>Je suis un artiste et un programmeur vivant à Montréal, Québec, Canada.</p>
+        <h2><i>Comment les choses prennent-elles leurs formes?</i></h2>
+        <p>Je suis un artiste visuel et un cinéaste d'animation vivant à Montréal, Québec, Canada.</p>
+        <p>Mon travail explore divers sujets mathématiques. Je suis fasciné par les mathématiques et
+        les divers procédés qui nous entourent : comment les arbres croissent et prennent leur forme,
+        comment les statistiques gouvernent silencieusement nos vies, comment il nous est possible
+        de construire des modèles des choses qui nous entourent afin de mieux les comprendre. 
+        <i>Comment les choses prennent-elles leurs formes?</i> est la question que je me pose 
+        constamment. Et qu'est-ce que la forme des choses révèle sur elles?</p>
+
+        <p>Je suis un grand anxieux et découvrir la beauté de la science m'a aidé à appréhender
+        le monde et à m'approprier davantage la petite place que j'y occupe. 
+        Et l'art me permet d'explorer et de communiquer ces sujets.</p>
         <p>
         J'ai d'abord étudié le film d'animation au Cégep du Vieux Montréal, puis l'illustration
-        à l'Académie d'été internationale des Beaux-Arts de Salzbourg, en Autriche. J'ai ensuite étudié
-        les arts graphiques à l'Académie des Beaux-Arts de Varsovie, en Pologne, me concentrant
-        sur l'illustration avec le professeur Zygmunt Januszewski et sur l'animation
-        avec le professeur Hieronim Neumann.
+        à l'Académie d'été internationale des Beaux-Arts de Salzbourg, en Autriche, et 
+        les arts graphiques à l'Académie des Beaux-Arts de Varsovie, en Pologne.
         </p>
+        <h2>Contact</h2>
+        N'hésitez pas à me contacter par courriel, sur Twitter ou sur GitHub pour toute question
+        concernant mon travail ou les divers sujets qui y sont explorés. Il me fait toujours
+        plaisir de discuter et de découvrir des points de vue différents.
+
+        <h2>Code source libre</h2>
+        <p>Les programmes que j'écris pour la création de mes œuvres sont tous offerts en code 
+        source libre, une chose qui me tient à cœur.</p>
+
+        <h2>Soutenir</h2>
+        <p>Si vous aimez mon travail et que vous désirez m'aider à continuer, je vous invite à
+        me soutenir sur la plateforme Patreon.</p>
+
+        <h2>Ce site Web</h2>
+
+
         </div>
     `
 };
