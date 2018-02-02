a94ec53879da5fd41cd0bfb0c6b000d68861571e
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-02-02 04:01:59 -0500

    Add refinements to make code-formatter.js color code punctuation

--- formatters/code-formatter.js
+++ formatters/code-formatter.js
@@ -23,9 +23,76 @@ module.exports = function(htmlContent, fileName) {
             two = two.replace(/^\s+|\s+$|\n+$/g, "");
             // console.log(two);
             var code = hljs.highlight("javascript", two);
+
+            // code.value = code.value.replace(/([a-zA-ZÀ-ú]\d*)(\.)(\d*[a-zA-ZÀ-ú])/g, function(match, a, b, c) {
+            //     return a + '<span class="hljs-punctuation">' + b + '</span>' + c;
+            // });
+            // code.value = code.value.replace(/^((?!hljs).)*$/g, function(match) {
+            //     console.log("YEAH!");
+            //     console.log(match);
+            // });
+            code.value = code.value.replace(/([>a-zA-ZÀ-ú]\d*)(\.)(\d*[a-zA-ZÀ-ú])/g, function(match, a, b, c) {
+                // console.log(match);
+                return a + '<span class="hljs-punctuation">' + b + '</span>' + c;
+            });
+            code.value = code.value.replace(/ = /g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/ != /g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/ \|\| /g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/ [:\?\*/] /g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/&lt;/g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/\+=/g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/\+\+/g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/ \+ /g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            code.value = code.value.replace(/ - /g, function(match) {
+                return '<span class="hljs-punctuation">' + match + '</span>';
+            });
+            for (var i = 0; i < 20; i++) {
+                code.value = code.value.replace(
+                    /(\/\/)([<"=>a-zA-ZÀ-ú\d\s\.\|-]*)(<span class="hljs-punctuation">)(.*?)(<\/span>)/g,
+                    function(match, a, b, c, d, e) {
+                        // console.log(a + b);
+                        return a + b + d;
+                    }
+                );
+            }
+            // code.value = code.value.replace(
+            //     /(\/\/)([<"=>a-zA-ZÀ-ú\d\s\.\|-]*)(<span class="hljs-punctuation">)(.*?)(<\/span>)/g,
+            //     function(match, a, b, c, d, e) {
+            //         console.log(a + b);
+            //         return a + b + d;
+            //     }
+            // );
+            // code.value = code.value.replace(
+            //     /(\/\/)([<"=>a-zA-ZÀ-ú\d\s\.\|-]*|<\/span>||<span class="hljs-punctuation">)(<span class="hljs-punctuation">)(.*?)(<\/span>)/g,
+            //     function(match, a, b, c, d, e) {
+            //         console.log(a + b);
+            //         return a + b + d;
+            //     }
+            // );
+
+            // code.value = code.value.replace(
+            //     new RegExp(`createCanvas|fill`, "g"),
+            //     function(match) {
+            //         return '<span class="hljs-p5">' + match + '</span>';
+            //     });
             return one + code.value + three;
         });
-
         // console.log(htmlContent)
 
         //If there is no code contained within the htmlContent
