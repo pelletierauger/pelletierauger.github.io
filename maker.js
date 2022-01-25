801523461f4186091b3b220d9e9973e5ffe1b98d
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2022-01-24 20:09:28 -0500

    Update the footer date from 2020 to 2022

--- maker.js
+++ maker.js
@@ -585,7 +585,7 @@ function makeContent(page, language) {
 }
 
 function makeFooter(language) {
-    return `<div id="footer">Guillaume Pelletier-Auger &mdash; 2016-2020
+    return `<div id="footer">Guillaume Pelletier-Auger &mdash; 2016-2022
     </div>
     </body>
     </html>`;
